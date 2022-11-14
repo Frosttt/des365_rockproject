2181d35c03dc9f54d82005c11c5abe76e1022c71
Chris Dowell <chrisdowell6@gmail.com>
2022-11-14 11:25:30 -0800

    e

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject ActorToFollow;
+
+    [Range(0, 20)]
+    public float SmoothSpeed = 10f;
+
+    public Vector3 offset = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 desiredPos = transform.position + offset;
+        Vector3 smooth = Vector3.Lerp(transform.position, desiredPos, SmoothSpeed * Time.deltaTime);
+        transform.position = smooth;
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    public GameObject Camera;
+    
+    public float MovementSpeed = 5.0f;
+    private bool FallDownPipe = false;
+    private Rigidbody rb;
+
+    private float ForwardAxis = 0;
+    private float RightAxis = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ForwardAxis = Input.GetAxisRaw("Vertical");
+        RightAxis = Input.GetAxisRaw("Horizontal");
+
+        Vector3 inputvector = new Vector3(RightAxis, 0, ForwardAxis);
+
+        rb.velocity = MovementSpeed * inputvector;
+    }
+}
